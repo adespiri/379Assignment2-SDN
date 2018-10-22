058253189a7dc1143a4d19356cd5ede757125128
Aaron Espiritu <adespiri@cs.ualberta.ca>
2018-10-21 18:07:58 -0600

    opened FIFOs

--- a2sdn.cpp
+++ a2sdn.cpp
@@ -13,7 +13,7 @@
 #include <fcntl.h>
 #include <poll.h> 
 #include <fstream>
-#include <sstream>
+#include <poll.h>
 
 #define MAX_NSW 7;
 #define MAX_IP 1000;
@@ -49,9 +49,11 @@ typedef struct {
 } Rule; //rules will be kept in a vector array as part of a switch
 
 typedef struct {
+	bool opened;
+	int switchNumber;
 	char switchIs[10];
-	char port1[10]; //may have to change from char to int
-	char port2[10];
+	int port1; 
+	int port2;
 	int IP_lo;
 	int IP_hi;
 	vector<Rule> rulesList;
@@ -66,15 +68,84 @@ typedef struct {
 
 } Switch; //switch struct
 
-void sendFrame(int fd, KIND kind, MSG *msg) //using lab exercise on eclass as reference TODO
-{
+typedef struct {
+	int openRcvCounter;
+	int queryRcvCounter;
+	int ackSentCounter;
+	int addSentCounter;
+	vector<Switch> connectedSwitches;
+} Controller; //controller struct to contain counters
 
+void sendFrame(int fd, KIND kind, MSG *msg) //using lab exercise on eclass as a reference
+{
+	FRAME frame;
+	assert(fd >= 0);
+	memset((char *)&frame, 0, sizeof(frame));
+	frame.kind = kind;
+	frame.msg = *msg;
+	write(fd, (char *)&frame, sizeof(frame));
+
+}
+
+FRAME rcvFrame(int fd)
+{ /*This function is taken from the lab exercise on eclass and is used to receive frames from FIFOs
+	fd is the file descriptor for the opened FIFO*/
+	int    len;
+	FRAME  frame;
+
+	assert(fd >= 0);
+	memset((char *)&frame, 0, sizeof(frame));
+	len = read(fd, (char *)&frame, sizeof(frame));
+	
+	return frame;
+}
+
+void printController(Controller cont)
+{ /* This method will print the controller details*/
+	printf("Switch Information: \n");
+	//for every switch that is connected, print its details
+
+	printf("\n");
+	printf("Packet Stats: \n");
+	printf("\tReceived:\tOPEN:%d, QUERY:%d\n", cont.openRcvCounter, cont.queryRcvCounter);
+	printf("\tTransmitted:\tACK:%d, ADD:%d\n\n", cont.ackSentCounter, cont.addSentCounter);
 }
 
 void executeController(int numberofSwitches)
 {	/* This method will be used for the instance that the controller is chosen*/
+	Controller cont;
+	cont.openRcvCounter = 0;
+	cont.queryRcvCounter = 0;
+	cont.ackSentCounter = 0;
+	cont.addSentCounter = 0;
 
+	char usercmd[30];
+	cout << "Controller Created" << endl;
 
+	while (1)
+	{
+		//poll the user for user command
+		cout << "Please type 'list' or 'exit': ";
+		cin >> usercmd;
+
+		if (strcmp(usercmd, "list") == 0)
+		{ 
+			printController(cont);
+		}
+
+		else if (strcmp(usercmd, "exit") == 0)
+		{
+			printController(cont);
+			return;
+		}
+
+		else 
+		{
+			cout << "Invalid Command" << endl; continue;
+		}
+	}	
+	
+	
 }
 
 Rule initializeRules(int lowIP, int highIP)
@@ -113,15 +184,39 @@ void printFlowTable(Switch sw)
 	printf("\t Transmitted:\tOPEN:%d, QUERY:%d, RELAYOUT:%d \n\n", sw.openCounter, sw.queryCounter, sw.relayOutCounter);
 }
 
-void executeSwitch(char* filename, char* port1, char* port2 , int lowIP, int highIP, char* thisSwitch)
+int openFIFO(int source, int destination)
+{	
+	char fifoString[20];
+
+	strcpy(fifoString, "fifo-x-y");
+	fifoString[5] = source + '0'; //convert to a character and replace x with source Number
+	fifoString[7] = destination + '0'; //do the same with y and replace it with destination
+	return open(fifoString, O_RDWR);
+}
+
+void sendOpenPacket(int CSfifo, int SCfifo, Switch sw)
+{ /*this method is called when a switch is initialized, it sends the open packet
+  to the controller and waits to receive the ACK packet*/
+
+}
+
+void executeSwitch(char* filename, int port1, int port2 , int lowIP, int highIP, char* thisSwitch, int switchNum)
 {	/* This method will be used for the instance that the switch is chosen*/
 	//First initialize the switch object
 	Switch sw;
 	char usercmd[20];
+	int CSfifo;
+	int SCfifo;
+	int p1writeFifo;
+	int p1readFifo;
+	int p2writeFifo;
+	int p2readFifo;
 
-	strcpy(sw.port1, port1);
-	strcpy(sw.port2, port2);
+	sw.opened = false;
+	sw.port1 =  port1;
+	sw.port2 = port2;
 	strcpy(sw.switchIs, thisSwitch);
+	sw.switchNumber = switchNum;
 	sw.IP_lo = lowIP;
 	sw.IP_hi = highIP;
 	sw.admitCounter = 0;
@@ -131,10 +226,27 @@ void executeSwitch(char* filename, char* port1, char* port2 , int lowIP, int hig
 	sw.openCounter = 0;
 	sw.queryCounter = 0;
 	sw.relayOutCounter = 0;
-	//initialize the first rule
+	//initialize the first rule 
 	sw.rulesList.push_back(initializeRules(lowIP, highIP));
 
+	//open up the FIFOs for this switch/controller pair as well as switch/switch pairs
+	CSfifo = openFIFO(sw.switchNumber, 0);
+	SCfifo = openFIFO(0, sw.switchNumber);
+	
+	if (sw.port1 != -1) //-1 indicates null and no connected switch to port 1
+	{
+		p1writeFifo = openFIFO(sw.switchNumber, sw.port1);
+		p1readFifo = openFIFO(sw.port1, sw.switchNumber);
+	}
+
+	if (sw.port2 != -1)
+	{
+		p2writeFifo = openFIFO(sw.switchNumber, sw.port2);
+		p2readFifo = openFIFO(sw.port2, sw.switchNumber);
+	}
+
 	//send open packet to controller
+	sendOpenPacket(CSfifo, SCfifo, sw);
 
 	string line;
 	ifstream file(filename); 
@@ -190,7 +302,7 @@ void executeSwitch(char* filename, char* port1, char* port2 , int lowIP, int hig
 
 int main(int argc, char* argv[])
 {
-	/*first thing we need to do is poll user for input. There are going to be two categories:
+	/*There are going to be two categories:
 	either the user initiates the program from the view of a controller or they do it from
 	the view of a switch
 	*/
@@ -210,6 +322,8 @@ int main(int argc, char* argv[])
 		char port1[10];
 		char port2[10];
 		char filename[100];
+		int port1num;
+		int port2num;
 
 		strcpy(port1, argv[3]);	//copying main line arguments into actual variables for easier readibility
 		strcpy(port2, argv[4]);
@@ -220,7 +334,26 @@ int main(int argc, char* argv[])
 		temp = strtok(NULL, "-");
 		highIP = atoi(temp);
 
-		executeSwitch(filename, port1, port2, lowIP, highIP, chosenSwitch);
+		//check if either of the ports is NULL
+		if (strcmp(argv[3], "null") == 0)
+		{
+			port1num = -1;
+		}
+		else
+		{	
+			port1num = atoi(&port1[2]);
+		}
+
+		if (strcmp(argv[4], "null") == 0)
+		{
+			port2num = -1;
+		}
+		else
+		{
+			port1num = atoi(&port2[2]);
+		}
+
+		executeSwitch(filename, port1num, port2num, lowIP, highIP, chosenSwitch, atoi(&chosenSwitch[2]));
 
 	}
 
